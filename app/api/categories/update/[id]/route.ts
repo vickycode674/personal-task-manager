a052bbadb41d2f330230f21629dd5cf227caff29
Chengalapattu Vivek Sai <chvivek674@gmail.com>
2025-03-14 18:47:35 +0530

    latest commit

--- app/api/categories/update/[id]/route.ts
+++ app/api/categories/update/[id]/route.ts
@@ -4,36 +4,36 @@ import { categories } from "@/drizzle/schema";
 import { z } from "zod";
 import { eq } from "drizzle-orm";
 
-
 export async function PUT(req: Request, { params }: { params: { id: string } }) {
-    try {
-      const body = await req.json();
-      
-      const categorySchema = z.object({
-        name: z.string().min(3, "Category name must be at least 3 characters"),
-      });
-    
-      
-      // Validate request
-      const validation = categorySchema.safeParse(body);
-      if (!validation.success) {
-        return NextResponse.json({ error: validation.error.format() }, { status: 400 });
-      }
-  
-      const categoryId = parseInt(params.id);
-      if (isNaN(categoryId)) {
-        return NextResponse.json({ error: "Invalid project ID" }, { status: 400 });
-      }
-  
-      const updatedCategory = await db.update(categories)
-        .set(body)
-        .where(eq(categories.id, categoryId))
-        .returning();
-  
-      return NextResponse.json({ message: "Project updated successfully", categories: updatedCategory }, { status: 200 });
-  
-    } catch (error) {
-      console.error("❌ Error updating project:", error);
-      return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  try {
+    const body = await req.json();
+
+    const categorySchema = z.object({
+      name: z.string().min(3, "Category name must be at least 3 characters"),
+    });
+
+    const validation = categorySchema.safeParse(body);
+    if (!validation.success) {
+      return NextResponse.json({ error: validation.error.format() }, { status: 400 });
     }
+
+    const categoryId = parseInt(params.id);
+    if (isNaN(categoryId)) {
+      return NextResponse.json({ error: "Invalid category ID" }, { status: 400 });
+    }
+
+    const updatedCategory = await db
+      .update(categories)
+      .set(body)
+      .where(eq(categories.id, categoryId))
+      .returning();
+
+    return NextResponse.json(
+      { message: "Category updated successfully", category: updatedCategory },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("❌ Error updating category:", error);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
   }
+}
